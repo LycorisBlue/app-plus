39fe88a237dd719e9d5bdb151cb61c4f3e2d6b61
lostvayne <lostvayneatful@gmail.com>
2023-08-21 14:46:46 +0000

    lost

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { About } from '../pages/About'
+import { Counter } from '../pages/Counter'
+import { Hero } from './Hero'
+import { Services } from '../pages/Services'
+import { Portfolio } from '../pages/Portfolio'
+import { Testimonials } from '../pages/Testimonials'
+import { Blog } from '../pages/Blog'
+import { Contact } from '../pages/Contact'
+import Footer from '../common/Footer'
+
+export const Home = () => {
+  return (
+    <>
+        <Hero/>
+        <About/>
+        <Services/>
+        <Counter/>
+        <Portfolio/>
+        <Testimonials/>
+        <Blog/>
+        <Contact/>
+    </>
+  )
+}
